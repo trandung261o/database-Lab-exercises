323d886c6332925d7e5729cbbcb5e048b5576da7
Tran Dinh Dung <114976992+trandung261o@users.noreply.github.com>
2024-03-24 01:34:31 +0700

    Update ex1_solution.sql

--- exercise 1/ex1_solution.sql
+++ exercise 1/ex1_solution.sql
@@ -22,3 +22,22 @@ CREATE TABLE ThamGia(
 	CONSTRAINT KhoaNgoai1 FOREIGN KEY (MaGV) REFERENCES GiangVien (MaGV),
 	CONSTRAINT KhoaNgoai2 FOREIGN KEY (MaDT) REFERENCES DeTai (MaDT)
 )
+
+INSERT INTO GiangVien VALUES('GV01',N'Vũ Tuyết Trinh',N'Hoàng Mai, Hà Nội','1975/10/10'),
+('GV02',N'Nguyễn Nhật Quang',N'Hai Bà Trưng, Hà Nội','1976/11/03'),
+('GV03',N'Trần Đức Khánh',N'Đống Đa, Hà Nội','1977/06/04'),
+('GV04',N'Nguyễn Hồng Phương',N'Tây Hồ, Hà Nội','1983/12/10'),
+('GV05',N'Lê Thanh Hương',N'Hai Bà Trưng, Hà Nội','1976/10/10')
+
+INSERT INTO DeTai VALUES ('DT01',N'Tính toán lưới',N'Nhà nước','700'),
+('DT02',N'Phát hiện tri thức',N'Bộ','300'),
+('DT03',N'Phân loại văn bản',N'Bộ','270'),
+('DT04',N'Dịch tự động Anh Việt',N'Trường','30')
+
+INSERT INTO ThamGia VALUES ('GV01','DT01','100'),
+('GV01','DT02','80'),
+('GV01','DT03','80'),
+('GV02','DT01','120'),
+('GV02','DT03','140'),
+('GV03','DT03','150'),
+('GV04','DT04','180')
