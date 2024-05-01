b5786e563993f3f4171915a1797d9671921caec0
Tran Dinh Dung <114976992+trandung261o@users.noreply.github.com>
2024-05-02 00:51:46 +0700

    Update dungtd_20215014.sql

--- exercise7/dungtd_20215014.sql
+++ exercise7/dungtd_20215014.sql
@@ -5,3 +5,8 @@ where published_year = 2020 and publisher = 'Wiley';
 --3. (1 đ) Liệt kê tổng số đầu sách trong thư viện cho từng nhà xuất bản (publisher, total)
 select book.publisher, sum(total_number_of_copies) as total from book
 group by book.publisher;
+
+--5. (1 đ) Liệt kê tất cả bạn đọc (id, name, telephone number, address) chưa trả sách.
+select Borrower.borrower_id, name, telephone_number, address 
+from Borrower join borrowcard using (borrower_id) 
+where actual_return_date is NULL;
