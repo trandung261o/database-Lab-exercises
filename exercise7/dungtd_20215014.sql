427b83e1cc5a7af43b2e169f8622f64932d52423
Tran Dinh Dung <114976992+trandung261o@users.noreply.github.com>
2024-05-02 00:44:05 +0700

    Update dungtd_20215014.sql

--- exercise7/dungtd_20215014.sql
+++ exercise7/dungtd_20215014.sql
@@ -2,4 +2,6 @@
 select * from book
 where published_year = 2020 and publisher = 'Wiley';
 
-
+--3. (1 đ) Liệt kê tổng số đầu sách trong thư viện cho từng nhà xuất bản (publisher, total)
+select book.publisher, sum(total_number_of_copies) as total from book
+group by book.publisher;
