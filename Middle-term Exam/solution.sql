d173567dd29199ae5231f4e443260749eb94eda6
Tran Dinh Dung <114976992+trandung261o@users.noreply.github.com>
2024-05-30 01:16:23 +0700

    Update solution.sql

--- Middle-term Exam/solution.sql
+++ Middle-term Exam/solution.sql
@@ -1,1 +1,33 @@
+--1
+SELECT prod_id, title 
+FROM products 
+WHERE category = (
+    SELECT category 
+    FROM categories 
+    WHERE categoryname = 'Documentary'
+);
 
+--2
+SELECT prod_id, title, price
+FROM products
+WHERE LOWER(title) LIKE '%apollo%' AND price < 10;
+
+--3
+SELECT c.category, c.categoryname
+FROM categories c
+LEFT JOIN products p ON c.category = p.category
+LEFT JOIN orderlines ol ON p.prod_id = ol.prod_id
+WHERE ol.prod_id IS NULL;
+
+--4
+SELECT DISTINCT country
+FROM customers c
+JOIN orders o ON c.customerid = o.customerid
+ORDER BY country;
+
+--5
+SELECT COUNT(*) AS number_of_customers
+FROM customers
+WHERE country = 'Germany';
+
+--6
